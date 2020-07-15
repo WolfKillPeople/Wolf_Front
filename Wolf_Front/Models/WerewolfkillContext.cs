35caa4104d68b562eac959f399c300452f1bddd2
a1256963 <a1256963@gmail.com>
2020-07-15 17:36:34 +0800

    test login

---
+++ Wolf_Front/Models/WerewolfkillContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Wolf_Front.Models
+{
+    public partial class WerewolfkillContext : DbContext
+    {
+        public WerewolfkillContext()
+        {
+        }
+
+        public WerewolfkillContext(DbContextOptions<WerewolfkillContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                //                optionsBuilder.UseSqlServer(" data source=werewolfkill.database.windows.net;initial catalog=Werewolfkill;persist security info=True;user id=Werewolfkill;password=Wolfpeoplekill_2020;MultipleActiveResultSets=True;");
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
