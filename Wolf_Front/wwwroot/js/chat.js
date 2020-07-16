93435b1a0c264875619921b8680070eb9f039c84
黃竣琛 <shooter19980521@gmail.com>
2020-07-16 15:52:43 +0800

    ChatRoom 3

--- Wolf_Front/wwwroot/js/chat.js
+++ Wolf_Front/wwwroot/js/chat.js
@@ -2,6 +2,7 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
+
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 document.getElementById("sendButton2").disabled = true;
@@ -14,8 +15,6 @@ function ChangeDay () {
     }
     else if (Day == "黑夜") {
         document.getElementById('background').style.backgroundColor = "gray";
-
-
     }
 }
 
@@ -24,8 +23,9 @@ connection.on("ReceiveMessage", function (user, message) {
     if (user == "人類" && Day == "白天") {
         document.getElementById("messagesList").hidden = false;
         document.getElementById("messagesList1").hidden = true;
+        var UserName = document.getElementById("Name").textContent;
         var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        var encodedMsg = user + " says " + msg;
+        var encodedMsg = UserName + " says " + msg;
         var li = document.createElement("li");
         li.textContent = encodedMsg;
         document.getElementById("messagesList").appendChild(li);
@@ -33,8 +33,9 @@ connection.on("ReceiveMessage", function (user, message) {
     else if (user == "狼人" && Day=="黑夜") {
         document.getElementById("messagesList1").hidden = false;
         document.getElementById("messagesList").hidden = false;
+        var UserName = document.getElementById("Name").textContent;
         var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        var encodedMsg = user + " says " + msg;
+        var encodedMsg = UserName + " says " + msg;
         var li = document.createElement("li");
         li.textContent = encodedMsg;
         document.getElementById("messagesList1").appendChild(li);
@@ -53,18 +54,18 @@ connection.start().then(function () {
 
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
+    var user = document.getElementById('userInput').value;
+    var message = document.getElementById('messageInput').value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
 });
 document.getElementById("sendButton2").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput2").value;
+    //var user = document.getElementById("userInput2").value;
+    var user = $('#userInput2').val()
+
     var message = document.getElementById("messageInput2").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
 });
