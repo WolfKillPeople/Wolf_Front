d79b983fe508f41c49eeb8a26e51d0f2cb630180
ginger <a0912870178@gmail.com>
2020-07-21 16:43:13 +0800

    前端完成，尚未與後端連結

--- Wolf_Front/wwwroot/js/RoomEnd.js
+++ Wolf_Front/wwwroot/js/RoomEnd.js
@@ -1,4 +1,4 @@
-
+﻿
 //door
 function openDoor(field) {
     var y = $(field).find(".thumb");
@@ -11,10 +11,103 @@ function openDoor(field) {
         y.addClass("thumbOpened");
     }
 }
-//$('#add_btn').click(addRoom());
-//function addRoom() {
 
-//}
+$(".add_room_btn").on("click", addDoor);
+var no = 1;
+var door_page = 1;
+var people = 0;
+// var countper = $(".perspective").length;
+let close_img = 'https://i.imgur.com/TGuCa7L.png';
+let open_img = '//i.imgur.com/582RIlF.png';
+let alt_close = 'close';
+let alt_open = 'open';
+
+var clicks = 0;
+function addDoor() {
+    let id = no.toString().padStart(3, '0');
+    clicks += 1;
+    if (door_page == 1 && clicks == 1) {
+        $(`.door_all`).append(`<div class="page page-${door_page} active">
+            <div class="half left">
+            <div class="perspective" onclick="openDoor(this)">
+                    <div class="thumb">
+                        <img src=${close_img} alt=${alt_close} class="door_card" />
+                    </div>
+                    <div class="number">
+                        <p class="door_number">${id}</p>
+                        <a href="#"><img src="https://i.imgur.com/V5A0Z92.gif" alt="wolf" class="wolf" /></a>
+                        <p class="people">人數: 3/10</p>
+                    </div>          
+                </div>
+            </div><div class="half right withText">
+            </div>
+            </div>`);
+        $('.scroll_ul').append(`<li data-target="${door_page}" class="nav-btn nav-page${door_page} active"></li>`)
+    }
+    else if ((clicks <= 4 && clicks > 1) || (clicks > 8 && clicks <= 12)) {
+        $(`.page-${door_page}>.left`).append(`<div class="perspective" onclick="openDoor(this)">
+                    <div class="thumb">
+                        <img src=${close_img} alt=${alt_close} class="door_card" />
+                    </div>
+                    <div class="number">
+                        <p class="door_number">${id}</p>
+                        <a href="#"><img src="https://i.imgur.com/V5A0Z92.gif" alt="wolf" class="wolf" /></a>
+                        <p class="people">人數: 3/10</p>
+                    </div>          
+                </div>`);
+    }
+    else if ((clicks > 4 && clicks <= 8) || (clicks > 12 & clicks <= 16)) {
+        $(`.page-${door_page}>.right`).append(`<div class="perspective" onclick="openDoor(this)">
+                    <div class="thumb">
+                        <img src=${close_img} alt=${alt_close} class="door_card" />
+                    </div>
+                    <div class="number">
+                        <p class="door_number">${id}</p>
+                        <a href="#"><img src="https://i.imgur.com/V5A0Z92.gif" alt="wolf" class="wolf" /></a>
+                        <p class="people">人數: 3/10</p>
+                    </div>          
+                </div>`);
+    }
+    else if (clicks == 17) {
+        door_page += 1;
+        clicks = 1;
+        $('.door_all').append(`<div class="page page-${door_page}">
+            <div class="half left">
+            <div class="perspective" onclick="openDoor(this)">
+                    <div class="thumb">
+                        <img src=${close_img} alt=${alt_close} class="door_card" />
+                    </div>
+                    <div class="number">
+                        <p class="door_number">${id}</p>
+                        <a href="#"><img src="https://i.imgur.com/V5A0Z92.gif" alt="wolf" class="wolf" /></a>
+                        <p class="people">人數: 3/10</p>
+                    </div>          
+                </div>
+            </div><div class="half right withText">
+            </div>
+            </div>`);
+
+        $('.scroll_ul').append(`<li data-target="${door_page}" class="nav-btn nav-page${door_page}"></li>`)
+    }
+    else if (door_page >= 2 && clicks == 1) {
+        $(`.door_all`).append(`<div class="page page-${door_page} active">
+            <div class="half left">
+            <div class="perspective" onclick="openDoor(this)">
+                    <div class="thumb">
+                        <img src=${close_img} alt=${alt_close} class="door_card" />
+                    </div>
+                    <div class="number">
+                        <p class="door_number">${id}</p>
+                        <a href="#"><img src="https://i.imgur.com/V5A0Z92.gif" alt="wolf" class="wolf" /></a>
+                        <p class="people">人數: 3/10</p>
+                    </div>          
+                </div>
+            </div><div class="half right withText">
+            </div>
+            </div>`);
+    }
+    no++;
+}
 
 
 
@@ -215,4 +308,95 @@ var triggerButton = document.getElementById('trigger');
 
 triggerButton.addEventListener('click', function () {
     myModal.open();
+});
+
+
+//page
+$(document).ready(function () {
+    // var pages = $(".page").length,
+    var scrolling = false,
+        curPage = 1;
+
+    function pagination(page, movingUp) {
+        scrolling = true;
+        var diff = curPage - page,
+            oldPage = curPage;
+        curPage = page;
+        $(".page").removeClass("active small previous");
+        $(".page-" + page).addClass("active");
+        $(".nav-btn").removeClass("active");
+        $(".nav-page" + page).addClass("active");
+        if (page > 1) {
+            $(".page-" + (page - 1)).addClass("previous");
+            if (movingUp) {
+                $(".page-" + (page - 1)).hide();
+                var hackPage = page;
+                setTimeout(function () {
+                    $(".page-" + (hackPage - 1)).show();
+                }, 600);
+            }
+            while (--page) {
+                $(".page-" + page).addClass("small");
+            }
+        }
+        console.log(diff)
+        if (diff > 1) {
+            for (var j = page + 1; j < oldPage; j++) {
+                $(".page-" + j + " .half").css("transition", "transform .7s ease-out");
+            }
+        }
+        setTimeout(function () {
+            scrolling = false;
+            $(".page .half").attr("style", "");
+            $(".page")
+        }, 700);
+    }
+
+    function navigateUp() {
+        if (curPage > 1) {
+            curPage--;
+            pagination(curPage, true);
+        }
+    }
+
+    function navigateDown() {
+        if (curPage < $(".page").length) {
+            curPage++;
+            pagination(curPage);
+        }
+    }
+
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+        if (!scrolling) {
+            if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+                navigateUp();
+            } else {
+                navigateDown();
+            }
+        }
+    });
+
+    $(document).on("click", ".scroll-btn", function () {
+        if (scrolling) return;
+        if ($(this).hasClass("up")) {
+            navigateUp();
+        } else {
+            navigateDown();
+        }
+    });
+
+    $(document).on("keydown", function (e) {
+        if (scrolling) return;
+        if (e.which === 38) {
+            navigateUp();
+        } else if (e.which === 40) {
+            navigateDown();
+        }
+    });
+
+    $(document).on("click", ".nav-btn:not(.active)", function () {
+        if (scrolling) return;
+        pagination(+$(this).attr("data-target"));
+    });
+
 });
