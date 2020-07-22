3e061c33ea2f931017fdf160cd4712771716306b
ginger <a0912870178@gmail.com>
2020-07-22 16:34:43 +0800

    get , post暫時完成

--- Wolf_Front/wwwroot/js/RoomEnd.js
+++ Wolf_Front/wwwroot/js/RoomEnd.js
@@ -1,4 +1,114 @@
-﻿
+﻿//page
+$(document).ready(function () {
+
+    let ary;
+    $.ajax({
+        type: 'GET',
+        url: 'https://wolfpeoplekill.azurewebsites.net/api/Room/CurrentRoom',
+        dataType: 'json',
+        contentType: 'application/json;charset=UTF-8',
+        success: function (msg) {
+            ary = msg;
+            var doorlength = ary.length;
+            alert(ary.length);
+            //console.log(msg);
+            
+            for (var i = 0; i < ary.length; i++) {
+                displayDoor();
+                no++;
+                for (var j = 0; j <= msg.length - 1; j++) {
+                    console.log(msg[j].roomId);
+                }
+            }
+        }
+    });
+    // var pages = $(".page").length,
+    var scrolling = false,
+        curPage = 1;
+
+    function pagination(page, movingUp) {
+        scrolling = true;
+        var diff = curPage - page,
+            oldPage = curPage;
+        curPage = page;
+        $(".page").removeClass("active small previous");
+        $(".page-" + page).addClass("active");
+        $(".nav-btn").removeClass("active");
+        $(".nav-page" + page).addClass("active");
+        if (page > 1) {
+            $(".page-" + (page - 1)).addClass("previous");
+            if (movingUp) {
+                $(".page-" + (page - 1)).hide();
+                var hackPage = page;
+                setTimeout(function () {
+                    $(".page-" + (hackPage - 1)).show();
+                }, 600);
+            }
+            while (--page) {
+                $(".page-" + page).addClass("small");
+            }
+        }
+        console.log(diff)
+        if (diff > 1) {
+            for (var j = page + 1; j < oldPage; j++) {
+                $(".page-" + j + " .half").css("transition", "transform .7s ease-out");
+            }
+        }
+        setTimeout(function () {
+            scrolling = false;
+            $(".page .half").attr("style", "");
+            $(".page")
+        }, 700);
+    }
+
+    function navigateUp() {
+        if (curPage > 1) {
+            curPage--;
+            pagination(curPage, true);
+        }
+    }
+
+    function navigateDown() {
+        if (curPage < $(".page").length) {
+            curPage++;
+            pagination(curPage);
+        }
+    }
+
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+        if (!scrolling) {
+            if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+                navigateUp();
+            } else {
+                navigateDown();
+            }
+        }
+    });
+
+    $(document).on("click", ".scroll-btn", function () {
+        if (scrolling) return;
+        if ($(this).hasClass("up")) {
+            navigateUp();
+        } else {
+            navigateDown();
+        }
+    });
+
+    $(document).on("keydown", function (e) {
+        if (scrolling) return;
+        if (e.which === 38) {
+            navigateUp();
+        } else if (e.which === 40) {
+            navigateDown();
+        }
+    });
+
+    $(document).on("click", ".nav-btn:not(.active)", function () {
+        if (scrolling) return;
+        pagination(+$(this).attr("data-target"));
+    });
+
+});
 //door
 function openDoor(field) {
     var y = $(field).find(".thumb");
@@ -13,6 +123,32 @@ function openDoor(field) {
 }
 
 $(".add_room_btn").on("click", addDoor);
+function addDoor() {
+    displayDoor();
+    $(function()
+    {
+
+        let jsonData = [
+            {
+                roomId: no,
+                player1: `${play1}`,
+            }
+        ];
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://wolfpeoplekill.azurewebsites.net/api/Room/AddRoom',
+            dataType: 'json',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify(jsonData),
+            success: function (msg) {
+                alert('Data Saved: ' + msg);
+                no++;
+            }
+        });
+
+    });
+}
 var no = 1;
 var door_page = 1;
 var people = 0;
@@ -23,9 +159,27 @@ let alt_close = 'close';
 let alt_open = 'open';
 
 var clicks = 0;
-function addDoor() {
+function displayDoor() {
+    
+    play1 = localStorage.getItem('myName');
     let id = no.toString().padStart(3, '0');
     clicks += 1;
+    $.ajax({
+        type: 'GET',
+        url: 'https://wolfpeoplekill.azurewebsites.net/api/Room/CurrentRoom',
+        dataType: 'json',
+        contentType: 'application/json;charset=UTF-8',
+        success: function (msg) {
+            ary = msg;
+            var doorlength = ary.length;
+            
+            for (var i = 0; i < ary.length; i++) {
+                for (var j = 0; j <= msg.length - 1; j++) {
+                    console.log(msg[j].length);
+                }
+            }
+        }
+    });
     if (clicks == 1) {
         $(`.door_all`).append(`<div class="page page-${door_page} active">
             <div class="half left">
@@ -89,33 +243,8 @@ function addDoor() {
 
         $('.scroll_ul').append(`<li data-target="${door_page}" class="nav-btn nav-page${door_page}"></li>`)
     }
-    no++;
-
-    $.ajax({
-        type: 'POST',
-        url: 'https://wolfpeoplekillapi.azurewebsites.net/api/Room/AddRoom',
-        data: [
-            {
-                "roomId": `${no}`,
-                "player1": "",
-                "player2": "",
-                "player3": "",
-                "player4": "",
-                "player5": "",
-                "player6": "",
-                "player7": "",
-                "player8": "",
-                "player9": "",
-                "player10": "",
-                "length": 0
-            }
-        ],
-        success: function (msg) {
-            alert('Data Saved: ' + msg);
-        }
-    });
+    
 }
-
 
 
 //sky
@@ -315,95 +444,4 @@ var triggerButton = document.getElementById('trigger');
 
 triggerButton.addEventListener('click', function () {
     myModal.open();
-});
-
-
-//page
-$(document).ready(function () {
-    // var pages = $(".page").length,
-    var scrolling = false,
-        curPage = 1;
-
-    function pagination(page, movingUp) {
-        scrolling = true;
-        var diff = curPage - page,
-            oldPage = curPage;
-        curPage = page;
-        $(".page").removeClass("active small previous");
-        $(".page-" + page).addClass("active");
-        $(".nav-btn").removeClass("active");
-        $(".nav-page" + page).addClass("active");
-        if (page > 1) {
-            $(".page-" + (page - 1)).addClass("previous");
-            if (movingUp) {
-                $(".page-" + (page - 1)).hide();
-                var hackPage = page;
-                setTimeout(function () {
-                    $(".page-" + (hackPage - 1)).show();
-                }, 600);
-            }
-            while (--page) {
-                $(".page-" + page).addClass("small");
-            }
-        }
-        console.log(diff)
-        if (diff > 1) {
-            for (var j = page + 1; j < oldPage; j++) {
-                $(".page-" + j + " .half").css("transition", "transform .7s ease-out");
-            }
-        }
-        setTimeout(function () {
-            scrolling = false;
-            $(".page .half").attr("style", "");
-            $(".page")
-        }, 700);
-    }
-
-    function navigateUp() {
-        if (curPage > 1) {
-            curPage--;
-            pagination(curPage, true);
-        }
-    }
-
-    function navigateDown() {
-        if (curPage < $(".page").length) {
-            curPage++;
-            pagination(curPage);
-        }
-    }
-
-    $(document).on("mousewheel DOMMouseScroll", function (e) {
-        if (!scrolling) {
-            if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-                navigateUp();
-            } else {
-                navigateDown();
-            }
-        }
-    });
-
-    $(document).on("click", ".scroll-btn", function () {
-        if (scrolling) return;
-        if ($(this).hasClass("up")) {
-            navigateUp();
-        } else {
-            navigateDown();
-        }
-    });
-
-    $(document).on("keydown", function (e) {
-        if (scrolling) return;
-        if (e.which === 38) {
-            navigateUp();
-        } else if (e.which === 40) {
-            navigateDown();
-        }
-    });
-
-    $(document).on("click", ".nav-btn:not(.active)", function () {
-        if (scrolling) return;
-        pagination(+$(this).attr("data-target"));
-    });
-
 });
