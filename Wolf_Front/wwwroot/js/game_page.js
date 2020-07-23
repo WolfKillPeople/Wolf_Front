3cf3ca0c66c9e74b9ecae894a40a0ea418ed1cbc
登凱 鍾 <lovemark2413@gmail.com>
2020-07-23 20:48:49 +0800

    .

--- Wolf_Front/wwwroot/js/game_page.js
+++ Wolf_Front/wwwroot/js/game_page.js
@@ -56,6 +56,153 @@ function update(percent, totaltime) {
     }
 }
 
+var players = [
+    {
+        "name": "狼人",
+        "imgUrl": "https://i.imgur.com/n7knadr.png",
+        "description": "黑夜可以睜眼與隊友見面並討論戰術與選擇殺害對象。狼人可以選擇當夜不殺害任何玩家（空刀）或自殺（自刀）。白天混入村落中混淆好人。狼人可以在白天任何時候選擇公布角色牌自我淘汰（自爆）強制進入黑夜階段，並在黑夜階段結束時離場。",
+        "isGood": false,
+        "roomId": 1,
+        "player": "string",
+        "alive": false
+    },
+    {
+        "name": "獵人",
+        "imgUrl": "https://i.imgur.com/TIvcUG5.png",
+        "description": "神職。除殉情或被毒殺外，以任何其他方式被淘汰時可以公布角色牌發動技能開槍帶走一位玩家，亦可以選擇壓槍不發動技能。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    },
+    {
+        "name": "女巫",
+        "imgUrl": "https://i.imgur.com/i9eRyug.png",
+        "description": "神職。擁有一瓶解藥和一瓶毒藥。解藥未使用時可以得知狼人的殺害對象，並決定是否救這一位玩家。然而，解藥全程不能用於解救自己。女巫也可以利用白天所得資訊，將懷疑的對象毒殺，該對象死後不能發動技能。解藥和毒藥不可以在同一夜使用。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": false
+    },
+    {
+        "name": "預言家",
+        "imgUrl": "https://i.imgur.com/8tiIFAB.png",
+        "description": "神職。每夜可以查驗一位存活玩家的所屬陣營，並在白天透過發言向好人報出資訊。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    },
+    {
+        "name": "村民",
+        "imgUrl": "https://i.imgur.com/D2o6MV6.png",
+        "description": "沒有特殊技能，黑夜階段全程閉眼，透過白天階段所得資訊投票放逐疑似狼人的玩家。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": false
+    },
+    {
+        "name": "村民",
+        "imgUrl": "https://i.imgur.com/4eJqZgk.png",
+        "description": "沒有特殊技能，黑夜階段全程閉眼，透過白天階段所得資訊投票放逐疑似狼人的玩家。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    },
+    {
+        "name": "狼王",
+        "imgUrl": "https://i.imgur.com/fVQQgnM.png",
+        "description": "又稱「狼槍」、「毒狼」。除殉情或被毒殺外，以任何其他方式被淘汰時可以發動技能帶走任何一位玩家。狼王在場時，獵人和黑狼王淘汰啟動技能均不公布角色牌。部分局式中，黑狼王自爆不能發動技能。",
+        "isGood": false,
+        "roomId": 1,
+        "player": "",
+        "alive": true
+    },
+    {
+        "name": "狼人",
+        "imgUrl": "https://i.imgur.com/n7knadr.png",
+        "description": "黑夜可以睜眼與隊友見面並討論戰術與選擇殺害對象。狼人可以選擇當夜不殺害任何玩家（空刀）或自殺（自刀）。白天混入村落中混淆好人。狼人可以在白天任何時候選擇公布角色牌自我淘汰（自爆）強制進入黑夜階段，並在黑夜階段結束時離場。",
+        "isGood": false,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    },
+    {
+        "name": "村民",
+        "imgUrl": "https://i.imgur.com/D2o6MV6.png",
+        "description": "沒有特殊技能，黑夜階段全程閉眼，透過白天階段所得資訊投票放逐疑似狼人的玩家。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    },
+    {
+        "name": "村民",
+        "imgUrl": "https://i.imgur.com/4eJqZgk.png",
+        "description": "沒有特殊技能，黑夜階段全程閉眼，透過白天階段所得資訊投票放逐疑似狼人的玩家。",
+        "isGood": true,
+        "roomId": 1,
+        "player": "string",
+        "alive": true
+    }
+];
+async function BindingPlayers() {
+var array = [];
+for (let i = 0; i < players.length / 2; i++) {
+    array.push(i + 1);
+    for (let j = 1; j <= players.length; j++) {
+        if (j != i + 1) { array.push(j); }
+    }
+    let aplayer = document.createElement('a');
+    let playerImg = document.createElement('img');
+    let dead = document.createElement('img');
+    let num = document.createElement('span');
+    let Circle = document.createElement('div');
+    num.innerHTML = i + 1;
+    num.setAttribute('class', 'number');
+    aplayer.setAttribute('class', 'playerimg')
+    aplayer.setAttribute('href', '#');
+    playerImg.setAttribute('src', players[i].imgUrl);
+    playerImg.setAttribute('class', 'playerphoto')
+    dead.setAttribute('src', 'https://i.imgur.com/OapUq4K.png');
+    dead.setAttribute('class', 'deadimg')
+    Circle.setAttribute('class', 'circleImg off');
+    Circle.appendChild(playerImg);
+    aplayer.appendChild(Circle);
+    aplayer.appendChild(num);
+    document.querySelector('.top-playerimg').appendChild(aplayer);
+    array = [];
+}
+var array = [];
+for (let i = players.length / 2; i <= players.length; i++) {
+    array.push(i + 1);
+    for (let j = 1; j <= players.length; j++) {
+        if (j != i + 1) { array.push(j); }
+    }
+    let aplayer = document.createElement('a');
+    let playerImg = document.createElement('img');
+    let dead = document.createElement('img');
+    let num = document.createElement('span');
+    let Circle = document.createElement('div');
+    num.innerHTML = i + 1;
+    num.setAttribute('class', 'number');
+    aplayer.setAttribute('class', 'playerimg')
+    aplayer.setAttribute('href', '#');
+    playerImg.setAttribute('src', players[i].imgUrl);
+    playerImg.setAttribute('class', 'playerphoto')
+    dead.setAttribute('src', 'https://i.imgur.com/OapUq4K.png');
+    dead.setAttribute('class', 'deadimg')
+    Circle.setAttribute('class', 'circleImg off');
+    Circle.appendChild(playerImg);
+    aplayer.appendChild(Circle);
+    aplayer.appendChild(num);
+    document.querySelector('.footer-playerimg').appendChild(aplayer);
+    array = [];
+    }
+}
+
 var wolf = function wolf() {
 
 }
@@ -72,7 +219,7 @@ let c = [wolf, witch, prophet, hunter];
 let whos = ['天黑請閉眼，狼人請殺人', '此玩家死亡，女巫是否救人', '預言家請選人查身分', '11651'];
 async function asyncArray() {
     $('#staticBackdrop').modal('show');
-    await timeOn(5);
+    await timeOn(3);
     Speak('講話');
     for (var i = 0; i <= c.length; i++) {
         Speak(whos[i])
@@ -80,6 +227,7 @@ async function asyncArray() {
     }
 }
 window.onload = function () {
-
+    //AJAX玩家資料
+    BindingPlayers();
     asyncArray();
 }
