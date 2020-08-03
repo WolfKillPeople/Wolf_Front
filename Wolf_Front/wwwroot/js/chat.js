78db68e3f7dc6c857434ef68f77618dedec2f60e
登凱 鍾 <lovemark2413@gmail.com>
2020-08-03 14:42:37 +0800

    merge

--- Wolf_Front/wwwroot/js/chat.js
+++ Wolf_Front/wwwroot/js/chat.js
@@ -1,13 +1,11 @@
 ﻿"use strict";
 
-
-
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//123
+
 //Disable send button until connection is established
-document.getElementById("PeoplesendButton").disabled = true;
-document.getElementById("WolfsendButton").disabled = true;
+document.getElementById("sendButton").disabled = true;
+document.getElementById("sendButton2").disabled = true;
 
 function ChangeDay() {
     var Day = document.getElementById("Day").value;
@@ -23,50 +21,44 @@ function ChangeDay() {
 connection.on("ReceiveMessage", function (user, message) {
     var Day = document.getElementById("Day").value;
     if (user == "人類" && Day == "白天") {
-        document.getElementById("PeoplemessagesList").hidden = false;
-        document.getElementById("WolfmessagesList").hidden = true;
-
-
+        document.getElementById("messagesList").hidden = false;
+        document.getElementById("messagesList1").hidden = true;
+        var UserName = document.getElementById("Name").textContent;
         var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        var encodedMsg = user + " says " + msg;
+        var encodedMsg = UserName + " says " + msg;
         var li = document.createElement("li");
         li.textContent = encodedMsg;
-        document.getElementById("PeoplemessagesList").appendChild(li);
+        document.getElementById("messagesList").appendChild(li);
     }
     else if (user == "狼人" && Day == "黑夜") {
         document.getElementById("messagesList1").hidden = false;
         document.getElementById("messagesList").hidden = false;
         var UserName = document.getElementById("Name").textContent;
         var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        var encodedMsg = user + " says " + msg;
+        var encodedMsg = UserName + " says " + msg;
         var li = document.createElement("li");
         li.textContent = encodedMsg;
-        document.getElementById("WolfmessagesList").appendChild(li);
+        document.getElementById("messagesList1").appendChild(li);
     }
 
 
 });
 
 connection.start().then(function () {
-    document.getElementById("PeoplesendButton").disabled = false;
-    document.getElementById("WolfsendButton").disabled = false;
+    document.getElementById("sendButton").disabled = false;
+    document.getElementById("sendButton2").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
 
 
-document.getElementById("PeoplesendButton").addEventListener("click", function (event) {
-    //var user = document.getElementById('userInput').value;
-    var message = document.getElementById('PeoplemessageInput').value;
-    var user;
-    if (message != "") {
-        user = "人類";
-        connection.invoke("SendMessage", user, message).catch(function (err) {
-            return console.error(err.toString());
-        });
-        document.getElementById('PeoplemessageInput').value = "";
-    }
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById('userInput').value;
+    var message = document.getElementById('messageInput').value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
 });
 document.getElementById("sendButton2").addEventListener("click", function (event) {
     //var user = document.getElementById("userInput2").value;
@@ -100,17 +92,12 @@ $('#Delete').click(function () {
     });
 });
 
-$('#GetAll').click(function() {
-    connection.invoke("GetAllRoom").then(function(response) {
+$('#GetAll').click(function () {
+    connection.invoke("GetAllRoom").then(function (response) {
         if (response.success) {
-            response.data.forEach(item=> {
-                console.log(item);  
+            response.data.forEach(item => {
+                console.log(item);
             });
         }
     })
 })
-
-
-
-
-
