b1c300da1f4f0764c71f3fbb802670bbb2c859fb
blake <aa4152@gmail.com>
2020-08-10 18:10:29 +0800

    modify

--- Wolf_Front/Startup.cs
+++ Wolf_Front/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
