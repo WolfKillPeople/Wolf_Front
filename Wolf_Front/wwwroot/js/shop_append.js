703b4373241d9672cea43d8b7366509ab11ffe3c
aquariusjou@gmail.com <aquariusjou@gmail.com>
2020-08-13 16:05:11 +0800

    BuyCard

--- Wolf_Front/wwwroot/js/shop_append.js
+++ Wolf_Front/wwwroot/js/shop_append.js
@@ -7,20 +7,19 @@ $('body').append(`
             <span></span>
         </div>
         
-        <div class="container">
+        <div class="container"><!--modal fade  tabindex="-1" aria-hidden="true" id="open_roleCard" aria-labelledby="open_roleCard"--!>
             <div class="row">
                 <div class="col-6">
                     <div class="card mb-3" style="max-width: 540px;">
                         <div class="row no-gutters">
                             <div class="col-md-4 ">
-                                <img src="https://i.imgur.com/knEt3u8.png" class="card-img-top " alt="civilian">
+                                <img src="https://i.imgur.com/knEt3u8.png" class="card-img-top" alt="civilian">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">角色:平民</h5>
-                                    <p class="card-text">描述:沒有任何技能，僅白天可以公投出心中的狼人。
-                                    </p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text">描述:沒有任何技能，僅白天可以公投出心中的狼人。</p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$250</small></p>
                                     <div class="cartBtn" >
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -40,8 +39,8 @@ $('body').append(`
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">角色:獵人</h5>
-                                    <p class="card-text">描述:在死亡時可以帶走一名玩家，在被毒死時不可以開槍。(也可以壓槍不帶人走)</p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text">描述:在死亡時可帶走一名玩家，在被毒死時不可以開槍。(也可以壓槍不帶人走)</p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$290</small></p>
                                     <div class="cartBtn" >
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -62,7 +61,7 @@ $('body').append(`
                                 <div class="card-body">
                                     <h5 class="card-title">角色:預言家</h5>
                                     <p class="card-text">描述:每晚可以驗證一名玩家身分，只能得知其身分為好人或狼人，無法確切得知其身分。</p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$340</small></p>
                                     <div class="cartBtn" >
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -83,7 +82,7 @@ $('body').append(`
                                 <div class="card-body">
                                     <h5 class="card-title">角色:狼人</h5>
                                     <p class="card-text">描述:夜間可以溝通，共同決定將殺害哪名玩家並商討戰術。</p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$490</small></p>
                                     <div class="cartBtn" >
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -98,13 +97,13 @@ $('body').append(`
                     <div class="card mb-3" style="max-width: 540px;">
                         <div class="row no-gutters">
                             <div class="col-md-4">
-                                <img src="https://i.imgur.com/W0DoEeE.png" class="card-img-top " alt="wolf_king">
+                                <img src="https://i.imgur.com/W0DoEeE.png" class="card-img-top" alt="wolf_king" style="width:134px;height:134px; margin-left:15px;margin-top:20px;">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">角色:狼王</h5>
-                                    <p class="card-text">描述:死亡後可以開槍殺死一名玩家，被毒則不可以開槍。(可以自爆帶人)</p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text">描述:死亡後可以開槍殺死一名玩家，被毒則不可以開槍。</p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$500</small></p>
                                     <div class="cartBtn">
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -125,7 +124,7 @@ $('body').append(`
                                 <div class="card-body">
                                     <h5 class="card-title">角色:女巫</h5>
                                     <p class="card-text">描述:有一瓶解藥與一瓶毒藥，一晚只能使用一瓶藥，可以自救。</p>
-                                    <p class="card-text"><small class="text-muted">價錢:$199</small></p>
+                                    <p class="card-text price"><small class="text-muted">價錢:&emsp;$390</small></p>
                                     <div class="cartBtn">
                                         <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
                                             <span class="cartText">Add to cart</span></a>
@@ -142,7 +141,7 @@ $('body').append(`
    <div class="side-bar">
         <ul>
            
-            <li><button class="ChooseBtn" id="roleBtn" onclick="RoleClick()">Role</button><li>
-            <li><button class="ChooseBtn" id="signBtn" >Sign in</button><li>
+            <li><button class="ChooseBtn" id="roleBtn"  onclick="RoleClick()">角色卡</button><li>   <!--data-toggle="modal" data-target="#open_roleCard"--!>
+            <li><button class="ChooseBtn" id="signBtn" >頭項選擇</button><li>
         </ul>
     </div>`)
